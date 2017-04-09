6552933c52f438ae4b325418522bb6ab0799debd
BolScript <admin@phuxuyen.net>
2017-04-09 15:00:05 +0700

    change http -> https

--- hosts/depositfiles_com.php
+++ hosts/depositfiles_com.php
@@ -1,44 +1,62 @@
 <?php
 
-class dl_depositfiles_com extends Download { 
-
-	public function CheckAcc($cookie){
-		//echo $data = $this->lib->curl("http://dfiles.eu/gold/payment_history.php", $cookie, ""); die();
-	//	$data = $this->lib->curl("http://dfiles.eu/gold/payment_history.php", $cookie, "");
-		$data = $this->lib->curl("http://depositfiles.com/gold/payment_history.php", $cookie, "");
-		if(stristr($data, 'You have Gold access until:'))  {
-			//$checksubscribe = $this->lib->curl("http://dfiles.eu/gold/payment_subscribe_manage.php", $cookie, "");
-		//	$checksubscribe = $this->lib->curl("http://depositfiles.com/gold/payment_subscribe_manage.php", $cookie, "");
-			//return array(true, "Until ".$this->lib->cut_str($data, '<div class="access">You have Gold access until: <b>','</b></div>') ."<br/> ". (strpos($checksubscribe, '>You are subscribed to automatically') ? "You are subscribed" : "You are not subscribed"));
-			return array(true, "(fix by hitpro)<br/>Until ".$this->lib->cut_str($data, '<div class="access">You have Gold access until: <b>','</b></div>'));
-		}
-		elseif(stristr($data, 'Your current status: FREE - member')) return array(false, "accfree");
-		else return array(false, "accinvalid");
-	}
-
-	public function Login($user, $pass){	
-		$data = $this->lib->curl("http://depositfiles.com/api/user/login?login=".urlencode($user)."&password=".urlencode($pass), '', '');
-		$cookie = "lang_current=en;".$this->lib->GetCookies($data);
-		return $cookie;
-	}
-
-	public function Leech($url) {
-		list($url, $test) = $this->linkpassword($url);
-		$tachid = explode("/", $url);  
-		if(preg_match("/\/files\/(.*)\/(.+)/i", $url, $id)) $DFid = $id[1];
-		elseif(count($tachid) == 5)  $DFid = $tachid[4];
-		else $DFid = $tachid[5];
-		$data = $this->lib->curl("http://depositfiles.com/api/download/file?&file_id=".$DFid."&file_password=".urlencode($test), $this->lib->cookie, "", 0);
-		$page = @json_decode($data, true);
-		if($page['status'] !== "OK" && $page['error'] == "FileIsPasswordProtected")  $this->error("reportpass", true, false);
-		elseif($page['status'] !== "OK" && $page['error'] == "FileDoesNotExist")   $this->error("dead", true, false, 2);
-		elseif($page['status'] !== "OK" && $page['error'] == "FilePasswordIsIncorrect")   $this->error("wrongpass", true, false, 2);
-		elseif($page['status'] == "OK" && isset($page['data']['download_url'])) return trim(str_replace("depositfiles.com", "dfiles.eu", $page['data']['download_url']));
-		else $this->error($page['error'], true, false);
-		return false;
-	}
-	
+class dl_depositfiles_com extends Download
+{
+    
+    public function CheckAcc($cookie)
+    {
+        $data = $this->lib->curl("https://depositfiles.com/gold/payment_history.php", $cookie, "");
+        if (stristr($data, 'You have Gold access until:')) {
+            return array(
+                true,
+                "(fix by hitpro)<br/>Until " . $this->lib->cut_str($data, '<div class="access">You have Gold access until: <b>', '</b></div>')
+            );
+        } elseif (stristr($data, 'Your current status: FREE - member'))
+            return array(
+                false,
+                "accfree"
+            );
+        else
+            return array(
+                false,
+                "accinvalid"
+            );
+    }
+    
+    public function Login($user, $pass)
+    {
+        $data   = $this->lib->curl("https://depositfiles.com/api/user/login?login=" . urlencode($user) . "&password=" . urlencode($pass), '', '');
+        $cookie = "lang_current=en;" . $this->lib->GetCookies($data);
+        return $cookie;
+    }
+    
+    public function Leech($url)
+    {
+        list($url, $test) = $this->linkpassword($url);
+        $tachid = explode("/", $url);
+        if (preg_match("/\/files\/(.*)\/(.+)/i", $url, $id))
+            $DFid = $id[1];
+        elseif (count($tachid) == 5)
+            $DFid = $tachid[4];
+        else
+            $DFid = $tachid[5];
+        $data = $this->lib->curl("https://depositfiles.com/api/download/file?&file_id=" . $DFid . "&file_password=" . urlencode($test), $this->lib->cookie, "", 0);
+        $page = @json_decode($data, true);
+        if ($page['status'] !== "OK" && $page['error'] == "FileIsPasswordProtected")
+            $this->error("reportpass", true, false);
+        elseif ($page['status'] !== "OK" && $page['error'] == "FileDoesNotExist")
+            $this->error("dead", true, false, 2);
+        elseif ($page['status'] !== "OK" && $page['error'] == "FilePasswordIsIncorrect")
+            $this->error("wrongpass", true, false, 2);
+        elseif ($page['status'] == "OK" && isset($page['data']['download_url']))
+            return trim(str_replace("depositfiles.com", "dfiles.eu", $page['data']['download_url']));
+        else
+            $this->error($page['error'], true, false);
+        return false;
+    }
+    
 }
+
 
 /*http://depositfiles.com/files/srfps4yz2
 * Open Source Project
