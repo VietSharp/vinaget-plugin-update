53314c2869f7aa55ac62cfd469c06c4cc4a607d9
BolScript <admin@phuxuyen.net>
2017-05-28 13:09:09 +0700

    aa

--- hosts/littlebyte_net.php
+++ hosts/littlebyte_net.php
@@ -1,17 +1,36 @@
 <?php
 
 class dl_littlebyte_net extends Download {
-   
+   	public function CheckAcc($cookie){
+		$json = str_replace("dummy=", "", $cookie);
+		$json = json_decode($json, true);
+		if($json["error"] !== 0) {
+			return [false, $json["message"]];
+		}
+		$data = $this->lib->curl("https://real-debrid.com/account", $json["cookie"], "");
+		if (strpos($data, '<strong>Free</strong>')) {
+			return [false, "accfree"];
+		} elseif (strpos($data, '<strong>Premium</strong>')) {
+			return [true, "Premium till: " . $this->lib->cut_str($data, "Valid untill: <strong>", "<")];
+		}
+		return [false, "accinvalid"];
+	}
+
+	public function Login($user, $pass){
+		$data = $this->lib->curl("https://real-debrid.com/ajax/login.php?user=".urlencode($user)."&pass=".urlencode(md5($pass))."&pin_challenge=&pin_answer=PIN%3A+000000&time=".time(), "", "", 0);
+		if (strpos($data, "}{")) $data = "{". $this->lib->cut_str($data, "}{", "}") . "}";
+		return "dummy=".$data;
+	}
 	public function Leech($url) {
 		$id = explode('/',$url);
 	$id = trim($id[4]);
-	$data = $this->curl("http://littlebyte.net/ajax.php","lng=EN","action=getLinks&params[file_id]={$id}&params[key]={$this->lib->cookie}");
+	$data = $this->curl("http://littlebyte.net/ajax.php","lng=EN","action=getLinks&params[file_id]={$id}&params[key]={$user}");
 	if(stristr($data,'Incorrect premium code')) $this->error("accinvalid", true, false);
 		elseif(preg_match('%a href=\'(.*)\' style=%U', $data, $redir)){
-		$link = str_replace(" ","%20",trim($redir[1]));
-		return $link;
+		return trim($redir[1]);
+		
 	}
-		return false;
+	//	return false;
 	}
 
 }
